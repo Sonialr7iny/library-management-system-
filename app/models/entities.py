67cbfd8c2beb1cd12a97e32482056992985feb40
Ruqia Faisal <unique0167@email.com>
2026-09-17 14:23:25 +0300

    feat: add Member model

--- app/models/entities.py
+++ app/models/entities.py
@@ -15,7 +15,7 @@ class Book:
     title: str
     author_name: str
     category: str
-    state : State =State.available
+    state : State =State.AVAILABLE
     id: str = field(default_factory=lambda: str(uuid4()))
 
     def to_dict(self) -> dict:
@@ -25,4 +25,23 @@ class Book:
     def from_dict(cls, data: dict) -> "Book":
         return cls(**data)
 
+
+@dataclass(slots=True)
+class Member:
+    name: str
+    phone_number: str
+    id: str = field(default_factory=lambda: str(uuid4()))
+
+    def __post_init__(self):
+        if not self.name:
+            raise ValueError("Name cannot be empty.")
+        if not self.phone_number:
+            raise ValueError("Phone number cannot be empty.")
+
+    def to_dict(self) -> dict:
+        return asdict(self)
+    
+    @classmethod
+    def from_dict(cls, data: dict) -> "Member":
+        return cls(**data)   
 

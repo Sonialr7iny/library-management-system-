eadb22766975a1aa6169af589799a35481ccb937
Ruqia Faisal <unique0167@email.com>
2026-09-17 22:17:21 +0300

    execeptions add storageError

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,16 +1,22 @@
 class LibraryException(Exception):
     """Base class for all library exceptions."""
 
+
 class ValidationError(LibraryException):
     pass
+
+
+class StorageError(LibraryException):
+    pass
+
 
 class BookNotFoundError(LibraryException):
     pass
 
+
 class MemberNotFoundError(LibraryException):
     pass
 
+
 class BookUnavailableError(LibraryException):
-    pass
-class storageError(LibraryException):
     pass

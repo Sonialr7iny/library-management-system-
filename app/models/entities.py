c26a24653a5028bab095c17b41f88b072fdac6f6
Khaled <alansyabwhrb@gmail.com>
2026-09-18 10:59:31 +0300

    fix:resolve errors in entities

--- app/models/entities.py
+++ app/models/entities.py
@@ -4,6 +4,8 @@ from dataclasses import asdict, dataclass, field
 from enum import StrEnum
 from uuid import uuid4
 
+from app.core.exceptions import ValidationError
+
 
 
 
@@ -54,16 +56,17 @@ class Member:
 class Loan:
     book_id: str
     member_id: str
-    borow_date: str
+    borrow_date: str
     return_date: str | None=None
+    id: str = field(default_factory=lambda: str(uuid4()))
 
     def __post_init__(self):
             if not self.book_id:
-                raise ValueError("Book id cannot be empty.")
+                raise ValidationError("Book id cannot be empty.")
             if not self.member_id:
-                raise ValueError("Member id cannot be empty.")
-            if not self.borow_date:
-                            raise ValueError("Borrow date cannot be empty.")
+                raise ValidationError("Member id cannot be empty.")
+            if not self.borrow_date:
+                raise ValidationError("Borrow date cannot be empty.")
 
     def to_dict(self) -> dict:
         return asdict(self)
